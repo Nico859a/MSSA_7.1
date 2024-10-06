e1bf5d7d09cfa7704d3f9cc0be6d8841a297113a
Nicholas Potenza <nicholas.potenza@outlook.com>
2024-10-05 21:17:59 -0500

    Add project files.

---
+++ Assignment7.1.2/MergeAlt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7._1._2
+{
+    internal class MergeAlt
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "abc";
+            string word2 = "pqr";
+            Console.WriteLine("The merged string will be merged as so: ");
+            Console.WriteLine(MergeStrings(word1, word2));
+
+            string word3 = "ab";
+            string word4 = "pqrs";
+            Console.WriteLine("The merged string will be merged as so: ");
+            Console.WriteLine(MergeStrings(word3, word4));
+        }
+
+        public static string MergeStrings(string word1, string word2)
+        {
+            StringBuilder sb = new StringBuilder(word1.Length + word2.Length);
+            int maxLength = Math.Max(word1.Length, word2.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < word1.Length)
+                {
+                    sb.Append(word1[i]);
+                }
+
+                if (i < word2.Length)
+                {
+                    sb.Append(word2[i]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Assignment7.1/SortedScores.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7._1
+{
+    internal class SortedScores
+    {
+        static void Main(string[] args)
+        {
+            int[] Scores = { 88, 76, 65, 94, 80, 95, 77, 68, 96, 84, 84, 55 };
+            SelectionSort(Scores);
+            Console.Write("The sorted exam scores are: ");
+            foreach (int i in Scores)
+            {
+                Console.Write(i);
+                Console.Write(",");
+            }
+            Console.ReadKey();
+        }
+
+        static void SelectionSort(int[] Scores)
+        {
+            int minposition = 0;
+            int temp = 0;
+            for (int i = 0; i < Scores.Length - 1; i++)
+            {
+                minposition = i;
+                for (int j = i + 1; j < Scores.Length; j++)
+                {
+                    if (Scores[j] < Scores[minposition])
+                    {
+                        minposition = j;
+                    }
+                }
+                if (minposition != i)
+                {
+                    temp = Scores[i];
+                    Scores[i] = Scores[minposition];
+                    Scores[minposition] = temp;
+                }
+            }
+        }
+    }
+}
